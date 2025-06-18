dc1c4d6783dafbbc333cde76fe9ecf4d538f758f
Daniel Peña <danielpenacruz@gmail.com>
2025-06-17 21:08:42 -0600

    Operaciones matematicas

--- dic.py
+++ dic.py
@@ -1,10 +1,17 @@
 DIC = [
-    "Que_tranza",
-    "Camara",
-    "Mi_carnal",
-    "Dice_que",
-    "y_echale",
-    "y_quitale",
-    "y_dice",
-    "Gritale"
+    "Que_tranza",           # Inicio del programa
+    "Camara",               # Fin del programa
+    "Mi_carnal",            # Declaración de varianle
+    "Dice_que",             # =
+    "y_echale",             # +
+    "y_quitale",            # -
+    "y_dice",               # 
+    "Llamale_a",              # Print(variable)
+    "no_le_llega_a",        # <
+    "esta_mas_chido_que",   # >
+    "Simon",                # True
+    "Nel",                  # False
+    "Apoco_si",             # If
+    "Apoco_no",             # EndIf
+    "Ahora_que_si_no"       # Else
 ]
--- main.py
+++ main.py
@@ -10,12 +10,23 @@ class Program:
     def tokenize(self, line):
         tokenized_line = line.strip().split(" ")
         
-        print(f'Tokens de la linea "{line}": {tokenized_line}')
+        #print(f'Tokens de la linea "{line}": {tokenized_line}')
         return tokenized_line
     
     def getMemo(self):
-        print(f'Memoria actual: {self.memo}')
+        #print(f'Memoria actual: {self.memo}')
         return
+    
+    
+    def parseValue(self, value):
+        if value.isdigit():
+            return int(value)
+        if value == "Simón":
+            return True
+        if value == "Nel":
+            return False
+        return value  # Asume string si no es ninguno de los anteriores
+
     
     
     def parseLine(self, tokens):
@@ -28,25 +39,53 @@ class Program:
             self.current_task.pop()
             return
             
+        # Programa Corriendo
         if self.current_task and self.current_task[-1] == "Start":
+            
+            # Declaración de variables
             if tokens[0] == "Mi_carnal" and tokens[2] == "dice_que":
-                self.memo[tokens[1]] = tokens[3]
+                self.memo[tokens[1]] = self.parseValue(tokens[3])
                 return
             
+            # Operaciones aritmeticas a variables
             if tokens[0] in self.memo:
+                
+                # Suma
                 if tokens[1] == "y_echale":
-                    new_val = int(self.memo[tokens[0]]) + int(tokens[2])
-                    self.memo[tokens[0]] = new_val
+                    val = self.memo[tokens[2]] if tokens[2] in self.memo else self.parseValue(tokens[2])
+                    self.memo[tokens[0]] = int(self.memo[tokens[0]]) + int(val)
+                    return
+
+                # Resta
+                if tokens[1] == "y_quitale":
+                    val = self.memo[tokens[2]] if tokens[2] in self.memo else self.parseValue(tokens[2])
+                    self.memo[tokens[0]] = int(self.memo[tokens[0]]) - int(val)
+                    return
+
+                # div
+                if tokens[1] == "y_partelo_en":
+                    val = self.memo[tokens[2]] if tokens[2] in self.memo else self.parseValue(tokens[2])
+                    self.memo[tokens[0]] = int(self.memo[tokens[0]]) / int(val)
                     return
                 
-                if tokens[1] == "y_quitale":
-                    new_val = int(self.memo[tokens[0]]) - int(tokens[2])
-                    self.memo[tokens[0]] = new_val
-                    return
             
-        if tokens[0] == "Gritale":
+            # Condicional 
+            if tokens[0] == "Apoco_si":
+                self.current_task.append("Cond")
+                
+                if tokens[1] == "no_le_llega_a":
+                    if (tokens[1] < tokens[-1]):
+                        pass
+                        
+            if tokens[0] == "Ahora_que_si_no" and not self.current_task[-1] == "Cond":
+                pass
+                        
+                
+            
+        # Print
+        if tokens[0] == "Llamale_a":
             item = self.memo[tokens[-1]]
-            print(item)
+            print(f'¡{item}, señitooo!')
             return
         
     def run(self, file_name):
@@ -56,8 +95,8 @@ class Program:
                     tokenized_line = self.tokenize(line)
                     self.parseLine(tokenized_line)
                     
-                    print(f'Memoria actual: {self.memo}')
-                    print(f"Acción actual: {self.current_task}")
+                    #print(f'Memoria actual: {self.memo}')
+                    #print(f"Acción actual: {self.current_task}")
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -68,12 +107,3 @@ if __name__ == "__main__":
     program = Program()
     program.run(archivo)
 
-
-
-# program.tokenize("Mi_carnal pulpo dice_que 5")
-# program.getMemo()
-# program.parseLine(program.tokenize("Mi_carnal pulpo dice_que 5"))
-# program.getMemo()
-# program.parseLine(program.tokenize("pulpo y_echale 10"))
-# program.getMemo()
-# program.parseLine(program.tokenize("Gritale pulpo"))
